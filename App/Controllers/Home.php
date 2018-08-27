b619f26826148393b405bc98327e31e84fcb1b8f
Malik Umer Farooq <Lablnet01@gmail.com>
2018-08-26 19:11:21 -0700

    Update Home.php

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -1,15 +1,11 @@
 <?php
 
 namespace App\Controllers;
-
-//for using Middleware
-use Zest\Middleware\Middleware;
 //for using View
 use Zest\View\View;
-//for using auth
+//for using auth management
 use Zest\Auth\Auth;
 use Zest\Auth\User;
-
 class Home extends \Zest\Controller\Controller
 {
     /**
@@ -20,12 +16,13 @@ class Home extends \Zest\Controller\Controller
     public function index()
     {
         $user = new User;
+        // in Auth user class there is method isLogin to check is user login or not
         if ($user->isLogin()) {
+            // in Auth user class there is method loginUser that return the login user array
             $args = $user->loginUser();
             View::View('account/profile',$args[0]);
         } else {
             View::view('account/signup');
         }
     }
-
 }
